64b5d4778aefd3741c588ca3dc7c319ca306cf94
GlitchHunt <huntj93@gmail.com>
2020-07-16 20:25:20 +0100

    added lint/prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>GlitchHunts' Blog</h1>
+      <h1>GlitchHunts&apos; Blog</h1>
       <p>16/07/2020</p>
-      <p>This is my first attempt at writing various bits and pieces down, documenting both my coding journey, as well as notes from my day/week</p>
+      <p>
+        This is my first attempt at writing various bits and pieces down, documenting both my coding
+        journey, as well as notes from my day/week
+      </p>
     </div>
   );
 }
