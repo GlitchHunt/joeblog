009d3216b51c1b997dcfed1bcd3a7ebcac9651bd
GlitchHunt <huntj93@gmail.com>
2020-07-17 15:51:16 +0100

    basic layout changes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>GlitchHunts&apos; Blog</h1>
-      <p>16/07/2020</p>
-      <p>
-        This is my first attempt at writing various bits and pieces down, documenting both my coding
-        journey, as well as notes from my day/week
-      </p>
+      <h1 className="global-header">GlitchHunts&apos; Blog</h1>
+      <div className="journal-entry-wrapper">
+        <p className="date">17/07/2020</p>
+        <p className="journal-entry">
+          I&apos;m now planning to build out this site into a private journal, taking inspiration
+          from several different applications. I will be mostly working on styling and continuing my
+          React tutorials for the most part, and slowly add features as I go!
+        </p>
+      </div>
+      <div className="journal-entry-wrapper">
+        <p className="date">16/07/2020</p>
+        <p className="journal-entry">
+          This is my first attempt at writing various bits and pieces down, documenting both my
+          coding journey, as well as notes from my day/week/month/year
+        </p>
+      </div>
     </div>
   );
 }
